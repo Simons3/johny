af643c13c4695bbfd3ed2861b46402dce8329169
Casondra Simons <casondra.simons@smusd90.org>
2024-04-04 09:15:29 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+import math
+
+name=input('What is the name of your first cousin?')
+name=name.swapcase()
+name=name[0:3]
+
+num=int(input("How many Galaxies do you think are nearest to ours?"))
+num=math.pow(num,2)*math.pi
+
+print('Your new password is:'+name+str(num))
